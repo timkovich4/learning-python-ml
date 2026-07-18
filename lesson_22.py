332be30010f14ac4471c0d7dd849c4ad55a58011
timkovich4 <t20030826as@gmail.com>
2026-07-18 10:23:43 +0700

    Изучил выживаемость в зависимости от пола и класса через groupby

---
+++ lesson_22.py
@@ -0,0 +1,12 @@
+import ssl
+import pandas as pd
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
+df = pd.read_csv(url)
+
+age_stats = df.groupby("Sex")["Survived"].agg(["mean", "median", "count"])
+mean_fare_by_class = df.groupby("Pclass")["Survived"].mean()
+print(mean_fare_by_class)
+print(age_stats)
