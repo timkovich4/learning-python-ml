4ee05561d0c5447e90d692aa8ac86a84c0a2c88c
timkovich4 <t20030826as@gmail.com>
2026-07-20 10:43:02 +0700

    Удалил Cabin и полностью заполнил пропуски в Titanic

--- lesson_28.py
+++ lesson_28.py
@@ -6,15 +6,17 @@ ssl._create_default_https_context = ssl._create_unverified_context
 url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
 df = pd.read_csv(url)
 
+# Удаляем столбец Cabin (columns=['Cabin'])
 df = df.drop(columns=["Cabin"])
 
-
+# Заполнение пропусков в столбце Embarked модой (частым значением)
+# .mode() возвращает Series, поэтому берем первый элемент [0]
 df['Embarked'] = df['Embarked'].fillna(df['Embarked'].mode()[0])
 
-
+# Заполнение пропусков в столбце Age медианой
 df["Age"] = df["Age"].fillna(df["Age"].median())
 
-
+# А так можно узнать процент пропусков, если поделить на общую длину таблицы
 missing_zero = (df.isna().sum() / len(df)) * 100
 print("\nПроцент пропусков по столбцам:")
 print(missing_zero)
