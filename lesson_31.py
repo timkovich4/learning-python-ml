6c71cc7f984327e7ae7e8db0c49e06dd6534bef4
timkovich4 <t20030826as@gmail.com>
2026-07-20 11:25:43 +0700

    Подготовил финальную матрицу признаков: удалил PassengerId, Name, Ticket

---
+++ lesson_31.py
@@ -0,0 +1,20 @@
+import ssl
+import pandas as pd
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
+df = pd.read_csv(url)
+
+# Базовая очистка (Cabin удаляем, Embarked модой, Age групповой медианой)
+df = df.drop(columns=["Cabin"])
+df['Embarked'] = df['Embarked'].fillna(df['Embarked'].mode()[0])
+df['Age'] = df['Age'].fillna(df.groupby(['Sex', 'Pclass'])['Age'].transform('median'))
+
+# Превращаем категориальные столбцы в бинарные столбцы 0 и 1 (One-Hot Encoding (OHE))
+df_encoded = pd.get_dummies(df, columns=["Sex", "Embarked"], dtype=int)
+
+# Удаляем столбцы, которые не несут прямой численной пользы для модели
+df_final = df_encoded.drop(columns=['PassengerId', 'Name', 'Ticket'])
+
+print(df_final.info())
